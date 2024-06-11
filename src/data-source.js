2c4f836eeb1394178af24492ec643c028f682e78
JoelTeoGom <joelteodorogomez@gmail.com>
2024-06-11 17:10:59 +0200

    correccio algun error syncro backend

--- src/data-source.js
+++ src/data-source.js
@@ -8,7 +8,7 @@ const AppDataSource = new DataSource({
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    synchronize: true,
+    synchronize: false,
     logging: false,
     entities: [
         'src/models/**/*.js'
